aaacd09f4be282ee2453dcbcf80cd21205cc95dd
Janus <Janus@SERVIDOR>
2025-06-06 23:13:12 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace arboles_estructura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_pregunta = new System.Windows.Forms.Label();
+            this.btn_si = new System.Windows.Forms.Button();
+            this.btn_no = new System.Windows.Forms.Button();
+            this.btn_reiniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_pregunta
+            // 
+            this.lbl_pregunta.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_pregunta.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pregunta.Location = new System.Drawing.Point(12, 9);
+            this.lbl_pregunta.Name = "lbl_pregunta";
+            this.lbl_pregunta.Size = new System.Drawing.Size(374, 59);
+            this.lbl_pregunta.TabIndex = 1;
+            
+            // 
+            // btn_si
+            // 
+            this.btn_si.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_si.FlatAppearance.BorderSize = 0;
+            this.btn_si.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_si.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_si.Location = new System.Drawing.Point(29, 97);
+            this.btn_si.Name = "btn_si";
+            this.btn_si.Size = new System.Drawing.Size(75, 56);
+            this.btn_si.TabIndex = 2;
+            this.btn_si.Text = "SI";
+            this.btn_si.UseVisualStyleBackColor = false;
+            this.btn_si.Click += new System.EventHandler(this.btn_si_Click);
+            // 
+            // btn_no
+            // 
+            this.btn_no.BackColor = System.Drawing.Color.Red;
+            this.btn_no.FlatAppearance.BorderSize = 0;
+            this.btn_no.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_no.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_no.Location = new System.Drawing.Point(289, 97);
+            this.btn_no.Name = "btn_no";
+            this.btn_no.Size = new System.Drawing.Size(75, 56);
+            this.btn_no.TabIndex = 3;
+            this.btn_no.Text = "NO";
+            this.btn_no.UseVisualStyleBackColor = false;
+            this.btn_no.Click += new System.EventHandler(this.btn_no_Click);
+            // 
+            // btn_reiniciar
+            // 
+            this.btn_reiniciar.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_reiniciar.FlatAppearance.BorderSize = 0;
+            this.btn_reiniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_reiniciar.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reiniciar.Location = new System.Drawing.Point(126, 463);
+            this.btn_reiniciar.Name = "btn_reiniciar";
+            this.btn_reiniciar.Size = new System.Drawing.Size(148, 56);
+            this.btn_reiniciar.TabIndex = 4;
+            this.btn_reiniciar.Text = "Reiniciar";
+            this.btn_reiniciar.UseVisualStyleBackColor = false;
+            this.btn_reiniciar.Click += new System.EventHandler(this.btn_reiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::arboles_estructura.Properties.Resources.OIP__5_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(398, 531);
+            this.Controls.Add(this.btn_reiniciar);
+            this.Controls.Add(this.btn_no);
+            this.Controls.Add(this.btn_si);
+            this.Controls.Add(this.lbl_pregunta);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_pregunta;
+        private System.Windows.Forms.Button btn_si;
+        private System.Windows.Forms.Button btn_no;
+        private System.Windows.Forms.Button btn_reiniciar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace arboles_estructura
+{
+    public partial class Form1 : Form
+    {
+        private NodoDecision raiz;
+        private NodoDecision nodoActual;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarArbolDesdeJson();
+            MostrarNodo();
+        }
+
+        private void CargarArbolDesdeJson()
+        {
+            string ruta = Path.Combine(Application.StartupPath, "arbol.json");
+
+            if (!File.Exists(ruta))
+            {
+                MessageBox.Show("No se encontró el archivo 'arbol.json'");
+                Application.Exit();
+                return;
+            }
+
+            string json = File.ReadAllText(ruta);
+            NodoDecision arbolCompleto = JsonConvert.DeserializeObject<NodoDecision>(json);
+
+            if (arbolCompleto == null)
+            {
+                MessageBox.Show("El archivo arbol.json no contiene un árbol válido.");
+                Application.Exit();
+                return;
+            }
+
+            // Obtener solo los nodos que tienen preguntas (que no sean hojas)
+            List<NodoDecision> nodosConPreguntas = ObtenerNodosConPreguntas(arbolCompleto);
+
+            if (nodosConPreguntas.Count > 0)
+            {
+                raiz = nodosConPreguntas[new Random().Next(nodosConPreguntas.Count)];
+                nodoActual = raiz;
+            }
+            else
+            {
+                MessageBox.Show("No hay suficientes preguntas en el árbol.");
+                Application.Exit();
+            }
+        }
+
+        // Método auxiliar para obtener nodos que tienen preguntas
+        private List<NodoDecision> ObtenerNodosConPreguntas(NodoDecision nodo)
+        {
+            List<NodoDecision> lista = new List<NodoDecision>();
+            Queue<NodoDecision> cola = new Queue<NodoDecision>();
+            cola.Enqueue(nodo);
+
+            while (cola.Count > 0)
+            {
+                NodoDecision actual = cola.Dequeue();
+
+                // Solo agregamos nodos que no sean hojas (que tengan pregunta)
+                if (!actual.EsHoja)
+                {
+                    lista.Add(actual);
+                }
+
+                if (actual.OpcionSi != null) cola.Enqueue(actual.OpcionSi);
+                if (actual.OpcionNo != null) cola.Enqueue(actual.OpcionNo);
+            }
+
+            return lista;
+        }
+
+
+        private void MostrarNodo()
+        {
+            if (nodoActual.EsHoja)
+            {
+                lbl_pregunta.Text = nodoActual.Recomendacion;
+                btn_si.Enabled = false;
+                btn_no.Enabled = false;
+            }
+            else
+            {
+                lbl_pregunta.Text = nodoActual.Pregunta;
+            }
+        }
+
+        private void btn_si_Click(object sender, EventArgs e)
+        {
+            if (nodoActual.OpcionSi != null)
+            {
+                nodoActual = nodoActual.OpcionSi;
+                MostrarNodo();
+            }
+        }
+
+        private void btn_no_Click(object sender, EventArgs e)
+        {
+            if (nodoActual.OpcionNo != null)
+            {
+                nodoActual = nodoActual.OpcionNo;
+                MostrarNodo();
+            }
+        }
+
+        private void btn_reiniciar_Click(object sender, EventArgs e)
+        {
+            if (nodoActual.EsHoja)
+            {
+                nodoActual = raiz;
+                btn_si.Enabled = true;
+                btn_no.Enabled = true;
+                MostrarNodo();
+            }
+            else
+            {
+                MessageBox.Show("Debes llegar a una recomendación antes de reiniciar.");
+            }
+        }
+    }
+
+    public class NodoDecision
+    {
+        public string Pregunta { get; set; }
+        public string Recomendacion { get; set; }
+        public NodoDecision OpcionSi { get; set; }
+        public NodoDecision OpcionNo { get; set; }
+
+        public bool EsHoja => OpcionSi == null && OpcionNo == null;
+    }
+}
